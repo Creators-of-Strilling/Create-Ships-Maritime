b82b47ba91976b8623cf03b867552c2717b96f56
Adrikikicp <adrikikicp@gmail.com>
2023-02-06 21:17:30 -0500

    Add most of the working stuff

---
+++ src/main/java/com/adrikikicp/ship/Ship.java
@@ -0,0 +1,7 @@
+package com.adrikikicp.ship;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("ship")
+public class Ship {
+}
---
+++ src/main/kotlin/com/adrikikicp/ship/ContraptionShip.kt
@@ -0,0 +1,8 @@
+package com.adrikikicp.ship
+
+import net.minecraft.world.entity.EntityType
+import net.minecraft.world.level.Level
+import org.valkyrienskies.mod.common.entity.ShipMountingEntity
+
+class ContraptionShip{
+}
